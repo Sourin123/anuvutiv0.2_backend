202a79b24798bde3a819eace29d4b6e2187f6e53
Sourin Saha <96835192+Sourin123@users.noreply.github.com>
2023-09-19 18:59:10 +0530

    hduyf

--- Routes/api.js
+++ Routes/api.js
@@ -12,7 +12,7 @@ router.get('/', function(req, res, next) {
           data : data
         }
         
-        res.json(getAllBooks)
+        res.send(toString(getAllBooks))
       } catch (error) {
         console.log(error);
       }
